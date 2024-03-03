babef48007c58d9ffb7bbbaba0ab476fec2b12c6
AKASH P C <iamakashpc@gmail.com>
2024-03-03 13:53:39 +0530

    app js modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-
 import Login from './pages/login/Login';
 import Home from './pages/home/Home';
 import List from './pages/list/List';
 import Single from './pages/single/Single';
+import New from './pages/new/New';
+import { productInputs, userInputs } from './formSource';
 function App() {
   return (
 		<BrowserRouter>
@@ -14,10 +15,18 @@ function App() {
 					<Route path="users">
 						<Route index element={<List />} />
 						<Route path=":userId" element={<Single />} />
+						<Route
+							path="new"
+							element={<New inputs={userInputs} title="Add New User" />}
+						/>
 					</Route>
 					<Route path="products">
 						<Route index element={<List />} />
 						<Route path=":productId" element={<Single />} />
+						<Route
+							path="new"
+							element={<New inputs={productInputs} title="Add New Product" />}
+						/>
 					</Route>
 				</Route>
 			</Routes>
